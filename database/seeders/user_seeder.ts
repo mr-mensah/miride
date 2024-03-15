106a600511017b274198b66921c9c988647876b8
Schandorf Osam-Frimpong <osamfrimpong@yahoo.com>
2024-03-15 03:50:28 +0000

    sanitised home pages

--- database/seeders/user_seeder.ts
+++ database/seeders/user_seeder.ts
@@ -3,7 +3,6 @@ import { BaseSeeder } from '@adonisjs/lucid/seeders'
 
 export default class extends BaseSeeder {
   async run() {
-    // Write your database queries inside the run method
     await User.createMany([
       {
         fullName: 'Test User',
